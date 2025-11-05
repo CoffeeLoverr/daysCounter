53e49339adcc37e9bfdf3cca6701e9897da80e3d
Anna <78046715+CoffeeLoverr@users.noreply.github.com>
2025-11-05 22:49:05 +0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const today = new Date();
-const arrivalDate = new Date("2025-11-18");
-today.setHours(0, 0, 0, 0);
-arrivalDate.setHours(0, 0, 0, 0);
-const diffMs = arrivalDate - today; // разница в миллисекундах
-const diffDays = diffMs / (1000 * 60 * 60 * 24);
-console.log(diffDays);
-console.log(today);
